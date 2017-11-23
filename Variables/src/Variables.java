de34e6046d83aae406e6ff971027940b6dead3f4
JonnakutiMahesh <33877380+JonnakutiMahesh@users.noreply.github.com>
2017-11-23 13:53:08 -0500

    Batch Commit

---
+++ Variables/src/Variables.java
@@ -0,0 +1,12 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double tuna;
+tuna = 5.28;
+System.out.println("tuna: " +tuna);
+System.out.print("tuna: " +tuna );
+	}
+
+}
